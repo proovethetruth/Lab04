ad447dc60c06f1027614afdc4c585a21a4e58a1c
seeker <populpock2@gmail.com>
2021-02-08 01:10:02 +0300

    Added "Match" function. Overall improvements

--- main.c
+++ main.c
@@ -8,10 +8,25 @@ int is_ch(char ch)
 	return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
 }
 
-int main() {
+int match(int *str, int *word, int i)
+{
+	int current = 0;
+	while (is_ch(str[i]))
+	{
+		if (str[i] != word[current])
+			return -1;
+		i++;
+		current++;
+	}
+	while (!is_ch(str[i]))
+		i++;
+	return i;
+}
 
+int main() 
+{
 	int a[100], b[10];
-	int i = 0, j = 0, length = 0;
+	int i = 0, j = 0, pos = 0;
 
 	printf("\n Input string: ");
 	while ((a[i] = getchar()) != '\n')
@@ -27,31 +42,37 @@ int main() {
 	}
 	b[j] = '\0';
 
-	// "never gongonna give you up\0"
-	// "gonna\0"
-	// "never gongonna give you up\0"
 	printf("\n -----------------------------------------------------\n Result: ");
-	for (i = 0, j = 0; a[i] != '\0'; i++)
+
+	i = 0;
+
+	// Если слова совпадают, функция должна вернуть целое j и записать в переменную pos
+
+	while (a[i] != '\0')
 	{
-		if (b[j] != '\0')
+		if ((pos = match(&a, &b, i)) < i)
 		{
-			if (a[i] == b[j])
-				j++;
-			else
+			while (is_ch(a[i]))
 			{
-				while (is_ch(a[i]))
-				{
-					printf("%c", a[i]);
-					i++;
-				}
-				j = 0;
+				printf("%c", a[i]);
+				i++;
+			}
+			while (!is_ch(a[i]) && a[i] != '\0')
+			{
+				printf("%c", a[i]);
+				i++;
 			}
 		}
 		else
 		{
-			printf("%c", a[i]);
+			i = pos;
+			while (!is_ch(a[i]))
+			{
+				printf("%c", a[i]);
+				i++;
+			}
 		}
-
+			
 	}
 	printf("\n");
 }
